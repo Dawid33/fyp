08994bfa5b179fd758048ad34a2d0229166f69cc
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-11-17 12:36:14 +0000

    desktop lit reveiw

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-rm *.out *.bbl *.fdb_latexmk *.glo *.lof *.log *.ist *.aux *.fls *.blg *.pdf *.toc *.synctex.gz *.glg *.gls
+rm *.acn *.acr *.alg *.out *.bbl *.fdb_latexmk *.glo *.lof *.log *.ist *.aux *.fls *.blg *.pdf *.toc *.synctex.gz *.glg *.gls
